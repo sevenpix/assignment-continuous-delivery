b13e2ba328f43795fca099ad6e2066c7464c834b
Mario Taferner <mario.taferner@gmail.com>
2017-04-21 13:40:01 +0200

    adds User validation

--- simple-rails-app/app/models/user.rb
+++ simple-rails-app/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+  validates :email, presence: true, uniqueness: true
 end
