2fff2905f9b86cc8e873ac1503a632975b9328fd
Mario Taferner <mario.taferner@gmail.com>
2017-04-21 15:34:26 +0200

    removed unnecesary gems

--- simple-rails-app/test/test_helper.rb
+++ simple-rails-app/test/test_helper.rb
@@ -3,12 +3,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails/capybara'
 
-# when using javascript-drivers and sqlite
-# you need https://github.com/iangreenleaf/transactional_capybara
-TransactionalCapybara.share_connection
-
-Capybara.javascript_driver = :webkit
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
