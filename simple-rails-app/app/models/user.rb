704ce67d9c13bfe04c43cda1dde4378adb62390d
Mario Taferner <mario.taferner@gmail.com>
2017-04-28 01:20:07 +0200

    delete simple-rails-app

--- simple-rails-app/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  validates :last_name, presence: true
-  validates :age, presence: true
-  validates :email, presence: true
-end
--- simple-rails-app/app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :name, :last_name, :age, :email, :created_at, :updated_at
-json.url user_url(user, format: :json)
